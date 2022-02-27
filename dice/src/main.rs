06342daad1fcb97b85b319687dd306a4d59f47be
Honda Ichigo <honda.ichigo1515@gmail.com>
2022-02-27 22:44:20 +0900

    FIX: challenge dice

--- dice/src/main.rs
+++ dice/src/main.rs
@@ -17,7 +17,7 @@ fn dice()
     println!("Please input a face of the dice.");
     io::stdin().read_line(&mut input).expect("Faild to read line.");
     let input_num: u32 = input.trim().parse().expect("Faild to parse input.");
-    if input_num > 6
+    if input_num > 6 || input_num < 1
     {
         panic!("Illegal face of dice.");
     }
