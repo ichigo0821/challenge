dfb74af4e1562cb2961a8fcf31c9e52e8d0d6d2c
Honda Ichigo <honda.ichigo1515@gmail.com>
2022-02-27 22:43:09 +0900

    ADD: challenge dice

---
+++ dice/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+fn main()
+{
+    println!(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    println!("The back of a dice");
+    println!(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+    dice();
+}
+
+fn dice()
+{
+    let dice_sum = 7;
+    let mut input = String::new();
+
+    println!("Please input a face of the dice.");
+    io::stdin().read_line(&mut input).expect("Faild to read line.");
+    let input_num: u32 = input.trim().parse().expect("Faild to parse input.");
+    if input_num > 6
+    {
+        panic!("Illegal face of dice.");
+    }
+
+    println!("The back of the dice is {}.", dice_sum - input_num);
+}
+
